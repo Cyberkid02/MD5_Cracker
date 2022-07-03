7303c565d9e408e32f44b752e8c63dd643cb6e07
Cyberkid <86096599+Cyberkid02@users.noreply.github.com>
2022-07-03 10:58:22 -0500

    Update md5cracker.py

--- md5cracker.py
+++ md5cracker.py
@@ -5,32 +5,41 @@ import sys
 
 
 def md5crack(hash_file, wordlists):
-   
+    HASHES = []
+    check = []
     try:
         hashdocument = open(hash_file, 'r')
     except IOError:
         print("Hash file not Found!")
         sys.exit()
     else:
-        crack = hashdocument.readline()
-        crack = crack.replace('\n', (""))
+        crack = hashdocument.readlines() # readlines() instead of readline() // will return a list
+        
+        print(f'\n[*] cracking {len(crack)} hashe(s)')
 
-        try:
-            wordlistfile = open(wordlists, 'r', encoding='latin1')
-        except IOError:
-            print("Wordlist file not found!")
-            sys.exit()
-        else:
-            for line in wordlistfile: # Reading through the wordlist.
-                md5crack = hashlib.md5()  # calling the algorithm type we want to crack
-                line = line.replace("\n", ("")) # removing any new lines character
-                md5crack.update(line.encode('utf-8')) # Updates the hash input by the words in the wordlist using 'utf-8' encoding
-                word_hash = md5crack.hexdigest() # convert the word to md5 hash
-                if word_hash == crack: # Checking if the word_hash equal to the hash 
-                    print(Fore.BLUE + f"\n[*] Great! The word match to the given hash is: {Fore.GREEN + line}")
-                    
-                else:
-                    pass
+        for HASH in crack:
+            crack = HASH.strip() # striping the hashes from the newlines
+            HASHES.append(crack)
+
+        for crack in HASHES:
+            print(f'\ncracking: {crack}')
+            try:
+                wordlistfile = open(wordlists, 'r', encoding='latin1').readlines()
+            except IOError:
+                print("Wordlist file not found!")
+                sys.exit()
+            else:
+                for line in wordlistfile: # Reading through the wordlist.
+                    line = line.strip() # removing any new lines character
+                    md5crack = hashlib.md5(line.encode())  # calling the algorithm type we want to crack
+                    word_hash = md5crack.hexdigest() # Updates the hash input by the words in the wordlist using 'utf-8' encoding
+                    if word_hash == crack: # Checking if the word_hash equal to the hash 
+                        # print(Fore.BLUE + f"\n[*] Great! The word match to the given hash is: {Fore.GREEN + line}")
+                        print(f'cracked: {line}')
+                        check.append('1')
+                    if len(check) == len(HASHES):
+                        exit('\n[*] Done ;)')
+                        
                 
 
 if __name__ == '__main__':      
@@ -38,8 +47,8 @@ if __name__ == '__main__':
         print(Fore.LIGHTMAGENTA_EX +"MD5 Crack\n")
         print(Fore.GREEN + "MD5 Hashes crack, store hashes & list in .txt\n")
         hash_file = input("[*] Name of Hashes File: ")
-        wordlists = input("[*] Name of the Wordlist: ")
-        md5crack(hash_file, wordlists)
+        wordlist = input("[*] Name of the Wordlist: ")
+        md5crack(hash_file, wordlist)
     except KeyboardInterrupt:
         print(Fore.RED + "\nExisting ...")
         sys.exit()
